cff9c554272a3334ba62820f493577a0689bf6c0
Juan Pedro <JuanP.95.torres@gmail.com>
2018-04-30 19:15:33 +0200

    PALETA

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -5,6 +5,16 @@
 
 #include "ui_mainwindow.h"
 
+struct color{
+	uchar r;
+	uchar g;
+	uchar b;
+	color(r_,g_,b_){
+		r=r_;
+		g=g_;
+		b=b_;
+	};
+};
 
 float normalizeFloat(float v)
 {
@@ -15,13 +25,40 @@ float normalizeFloat(float v)
 	return v;
 }
 
-uint8_t ucharize(float v)
+//AQUI AÑADIR LA PALETA
+color ucharize(float v)
 {
+	std::map<float,color> paleta;
+	paleta[0.0]= color(0,0,80);
+	paleta[39.23]= color(0,30,100);
+	paleta[78.46]= color(0,50,102);
+	paleta[107.88]= color(19,108,160);
+	paleta[153.98]= color(24,140,205);
+	paleta[155.94]= color(135,206,250);
+	paleta[156.73]= color(176,226,255);
+	paleta[156.00]= color(0,97,71);
+	paleta[157.90]= color(16,122,47);
+	paleta[166.73]= color(232,215,125);
+	paleta[180.46]= color(161,67,0);	
+	paleta[192.23]= color(130,30,30);
+	paleta[211.85]= color(161,161,161);
+	paleta[235.38]= color(206,206,206);
+	paleta[255.00]= color(255,255,255);
 	if (v > 255)
-		return 255;
+		return color(255,255,255);
 	if (v < 0)
-		return 0;
-	return (uint8_t)v;
+		return color(0,0,80);
+	color prev;
+	for (auto const& fila : paleta)
+	{
+    		if (fila.first > v){
+			return prev;		
+		}
+		else{
+			prev = fila.second;
+		}
+	}
+	return color(255,255,255);
 }
 
 pcl::PointCloud< pcl::PointXYZRGBA >::Ptr copy_pointcloud(pcl::PointCloud< pcl::PointXYZRGBA >::Ptr cloud)
@@ -35,7 +72,7 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWi
 	setWindowFlags(Qt::Window | Qt::FramelessWindowHint);
 	ui->setupUi(this);
 	show();
-
+	
 	cv_image = cv::Mat(480, 640, CV_8UC3);
 	osgImage = NULL;
 	rgb = new uint8_t[640*480*3];
@@ -46,7 +83,7 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWi
 	cloud = NULL;
 
 	viewer = new pcl::visualization::CloudViewer("cloud");
-
+   
 	connect(&timerOSG, SIGNAL(timeout()), this, SLOT(computeOSG()));
 	timerOSG.start(2);
 	connect(ui->loadButton, SIGNAL(clicked()), this, SLOT(button_slot()));
@@ -66,7 +103,7 @@ void MainWindow::initCamera()
 {
 #ifdef READ_DATA_FROM_DEVICE
 	int tries = 0;
-
+	
 	while (tries < 3)
 	{
 		try
@@ -96,7 +133,7 @@ void MainWindow::initCamera()
 			sleep(2);
 		}
 	}
-
+	
 	if (tries)
 	{
 		exit(-1);
@@ -122,18 +159,18 @@ void MainWindow::init3D()
 	osg::Geode* shapeGeode = new osg::Geode();
 	pat->addChild(shapeGeode);
 
-
+	
 	float k = 1.3;
 	osg::Box *box = new osg::Box( osg::Vec3(0, 0, 0), -10.24*k, 7.68*k, 0.01);
 	osg::ShapeDrawable *boxDrawable = new osg::ShapeDrawable(box);
  	boxDrawable->setColor(osg::Vec4(1,1,1,1));
 	shapeGeode->addDrawable(boxDrawable);
-
+	
 	osgImage = new osg::Image;
 	osgTexture = new osg::Texture2D;
 	bStateSetIMAGEN = shapeGeode->getOrCreateStateSet();
 	bStateSetIMAGEN->setTextureMode(0, GL_TEXTURE_GEN_R, osg::StateAttribute::ON);
-
+	
 }
 
 
@@ -145,7 +182,7 @@ void MainWindow::computeOSG()
 	cvWaitKey(1);
 
 
-	osgImage->setImage(640, 480, 1, GL_RGB, GL_RGB, GL_UNSIGNED_BYTE, bb, osg::Image::NO_DELETE);
+	osgImage->setImage(640, 480, 1, GL_RGB, GL_RGB, GL_UNSIGNED_BYTE, bb, osg::Image::NO_DELETE); 
 	osgTexture->setImage(0, osgImage);
 	bStateSetIMAGEN->setTextureAttributeAndModes(0, osgTexture, osg::StateAttribute::ON);
 	osgw->frame();
@@ -178,12 +215,12 @@ void MainWindow::computeImages2(const boost::shared_ptr<pcl::io::openni2::DepthI
 {
 	static uint32_t count = 0;
 	count++;
-
+	
 	if (osgImage == NULL)
 		return;
 
 	dimg->fillDepthImage(640, 480, bf);
-
+	
 	drawColors();
 }
 
@@ -196,9 +233,9 @@ void MainWindow::readPCD()
 	if (osgImage == NULL)
 		return;
 
-	if (pcl::io::loadPCDFile<pcl::PointXYZRGBA> (std::string("../data")+std::to_string(ui->spinBox->value())+std::string(".pcd"), *cloud) == -1)
+	if (pcl::io::loadPCDFile<pcl::PointXYZRGBA> (std::string("../data1")+std::to_string(ui->spinBox->value())+std::string(".pcd"), *cloud) == -1)
 	{
-		PCL_ERROR ("Couldn't read file pcd: %s\n", (std::string("../data")+std::to_string(ui->spinBox->value())+std::string(".pcd").c_str()));
+		PCL_ERROR ("Couldn't read file pcd: %s\n", (std::string("../data1")+std::to_string(ui->spinBox->value())+std::string(".pcd").c_str()));
 		exit(1);
 	}
 	viewer->showCloud(cloud);
@@ -210,8 +247,8 @@ void MainWindow::readDepth()
 		return;
 
 	printf("depth\n");
-	readBufferFromFile(std::string("../data")+std::to_string(ui->spinBox->value())+std::string(".depth"), bf, 640*480*4);
-	readBufferFromFile(std::string("../data")+std::to_string(ui->spinBox->value())+std::string(".rgb"), rgb, 640*480*3);
+	readBufferFromFile(std::string("../data1")+std::to_string(ui->spinBox->value())+std::string(".depth"), bf, 640*480*4);
+	readBufferFromFile(std::string("../data1")+std::to_string(ui->spinBox->value())+std::string(".rgb"), rgb, 640*480*3);
 	drawColors();
 }
 #endif
@@ -237,14 +274,14 @@ void MainWindow::drawColors()
 	float maxP = -1;
 	float min = -1;
 	float max = -1;
-
+	
 	for (uint32_t r=0; r<480; r++)
 	{
 		for (uint32_t c=0; c<640; c++)
 		{
 			const uint32_t idx = r*640+c;
 			float v = bf[idx];
-
+			
 			if (std::isnan(v))
 				continue;
 			if (v>maxP)
@@ -255,7 +292,7 @@ void MainWindow::drawColors()
 			float minV = 0.75;
 			float maxV = 1.1;
 
-
+			
 			if (v<minV)
 			{
 				bb[idx*3 + 0] = bb[idx*3 + 1] = bb[idx*3 + 2] = 0;
@@ -265,10 +302,10 @@ void MainWindow::drawColors()
 				v -= minV;
 				v /= maxV-minV;
 				v = normalizeFloat(v*255);
-
-				bb[idx*3 + 0] = (uint8_t)ucharize(v);
-				bb[idx*3 + 1] = (uint8_t)ucharize(v);
-				bb[idx*3 + 2] = (uint8_t)ucharize(v);
+				color c = ucharize(v);
+				bb[idx*3 + 0] = (uint8_t)c.r;
+				bb[idx*3 + 1] = (uint8_t)c.g;
+				bb[idx*3 + 2] = (uint8_t)c.b;
 			}
 		}
 	}
